1e400c1c3b113229ac1c832ed355ccdfbe1f599d
Joao Paulo <joaopauloj405@gmail.com>
2023-10-28 11:13:43 -0300

    feat: add server identity

--- server.js
+++ server.js
@@ -1,23 +1,39 @@
-const net = require('net');
+const net = require("net");
 
 const server = net.createServer((socket) => {});
+const clients = [];
 
 server.on("connection", (socket) => {
   console.log("New client connected");
 
+  const clientId = clients.length + 1;
+  clients.map((client) => {
+    client.socket.write(`User ${clientId} joined!`);
+  });
+
+  socket.write(`id-${clientId}`);
+
   socket.on("data", (data) => {
-    console.log(data.toString("utf-8"));
+    const dataString = data.toString("utf-8");
+    const id = dataString.substring(0, dataString.indexOf("-"));
+    const message = dataString.substring(dataString.indexOf("-message-") + 9);
+
+    clients.map((client) => {
+      client.socket.write(`> User ${id}: ${message}`);
+    });
   });
 
   socket.on("end", () => {
-    console.log("Client disconnected");
+    socket.on("end", () => {
+      clients.map((client) => {
+        client.socket.write(`User ${clientId} left!`);
+      });
+    });
   });
 
-  socket.write("Welcome to the server");
+  clients.push({ id: clientId.toString(), socket });
 });
 
-
-
 server.listen(3008, "127.0.0.1", () => {
-  console.log('TCP Server is running on ', server.address());
+  console.log("TCP Server is running on ", server.address());
 });
